0614e5cdd548109656e505aeba8f3ba4074facb7
Your Name <Sumitpawara02@gmail.com>
2022-12-01 14:23:46 +0530

    Check Prime Number

---
+++ prime.js
@@ -0,0 +1,10 @@
+ count=0;
+    for(i=1;i<=num;i++){
+        if(num%i==0 && num%num==0){
+            count++;
+        }
+    }if(count!==2){
+        console.log("Its a Prime Number");
+    }else{
+        console.log("Its Not a Prime Number");
+    }
